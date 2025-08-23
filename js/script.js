1727d482567149b041ba9b6af3cc2a31c929eb94
EonPro <57166328+defenderof516@users.noreply.github.com>
2025-08-23 09:30:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,23 @@
-$(document).ready(function(){
 
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
-  
-    $(window).on('scroll load',function(){
-  
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('toggle');
-  
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
-  
-    });
-  
-    // smooth scrolling 
-  
-    $('a[href*="#"]').on('click',function(e){
-  
-      e.preventDefault();
-  
-      $('html, body').animate({
-  
-        scrollTop : $($(this).attr('href')).offset().top,
-  
-      },
-        500, 
-        'linear'
-      );
-  
-    });
-  
+document.addEventListener('DOMContentLoaded', function(){
+  var toggle = document.getElementById('nav-toggle');
+  var nav = document.getElementById('nav');
+  toggle.addEventListener('click', function(){
+    nav.classList.toggle('open');
   });
+
+  // close nav on link click (mobile)
+  document.querySelectorAll('#nav a').forEach(function(a){
+    a.addEventListener('click', function(){ nav.classList.remove('open'); });
+  });
+
+  // smooth scroll
+  document.querySelectorAll('a[href^="#"]').forEach(function(a){
+    a.addEventListener('click', function(e){
+      e.preventDefault();
+      var id = this.getAttribute('href').slice(1);
+      var el = document.getElementById(id);
+      if(el) el.scrollIntoView({behavior:'smooth', block:'start'});
+    });
+  });
+});
